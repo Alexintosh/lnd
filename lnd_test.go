aeac2ceb2c876128ca29a762954d609d52eadc22
Johan T. Halseth <johanth@gmail.com>
2018-04-19 13:52:23 +0200

    comment tests

--- lnd_test.go
+++ lnd_test.go
@@ -8181,163 +8181,163 @@ type testCase struct {
 }
 
 var testsCases = []*testCase{
-	{
-		name: "basic funding flow",
-		test: testBasicChannelFunding,
-	},
-	{
-		name: "update channel policy",
-		test: testUpdateChannelPolicy,
-	},
-	{
-		name: "open channel reorg test",
-		test: testOpenChannelAfterReorg,
-	},
-	{
-		name: "disconnecting target peer",
-		test: testDisconnectingTargetPeer,
-	},
-	{
-		name: "graph topology notifications",
-		test: testGraphTopologyNotifications,
-	},
-	{
-		name: "funding flow persistence",
-		test: testChannelFundingPersistence,
-	},
-	{
-		name: "channel force closure",
-		test: testChannelForceClosure,
-	},
-	{
-		name: "channel balance",
-		test: testChannelBalance,
-	},
-	{
-		name: "single hop invoice",
-		test: testSingleHopInvoice,
-	},
-	{
-		name: "sphinx replay persistence",
-		test: testSphinxReplayPersistence,
-	},
-	{
-		name: "list outgoing payments",
-		test: testListPayments,
-	},
-	{
-		name: "max pending channel",
-		test: testMaxPendingChannels,
-	},
-	{
-		name: "multi-hop payments",
-		test: testMultiHopPayments,
-	},
-	{
-		name: "private channels",
-		test: testPrivateChannels,
-	},
-	{
-		name: "multiple channel creation",
-		test: testBasicChannelCreation,
-	},
-	{
-		name: "invoice update subscription",
-		test: testInvoiceSubscriptions,
-	},
-	{
-		name: "multi-hop htlc error propagation",
-		test: testHtlcErrorPropagation,
-	},
-	// TODO(roasbeef): multi-path integration test
-	{
-		name: "node announcement",
-		test: testNodeAnnouncement,
-	},
-	{
-		name: "node sign verify",
-		test: testNodeSignVerify,
-	},
+	//	{
+	//		name: "basic funding flow",
+	//		test: testBasicChannelFunding,
+	//	},
+	//	{
+	//		name: "update channel policy",
+	//		test: testUpdateChannelPolicy,
+	//	},
+	//	{
+	//		name: "open channel reorg test",
+	//		test: testOpenChannelAfterReorg,
+	//	},
+	//	{
+	//		name: "disconnecting target peer",
+	//		test: testDisconnectingTargetPeer,
+	//	},
+	//	{
+	//		name: "graph topology notifications",
+	//		test: testGraphTopologyNotifications,
+	//	},
+	//	{
+	//		name: "funding flow persistence",
+	//		test: testChannelFundingPersistence,
+	//	},
+	//	{
+	//		name: "channel force closure",
+	//		test: testChannelForceClosure,
+	//	},
+	//	{
+	//		name: "channel balance",
+	//		test: testChannelBalance,
+	//	},
+	//	{
+	//		name: "single hop invoice",
+	//		test: testSingleHopInvoice,
+	//	},
+	//	{
+	//		name: "sphinx replay persistence",
+	//		test: testSphinxReplayPersistence,
+	//	},
+	//	{
+	//		name: "list outgoing payments",
+	//		test: testListPayments,
+	//	},
+	//	{
+	//		name: "max pending channel",
+	//		test: testMaxPendingChannels,
+	//	},
+	//	{
+	//		name: "multi-hop payments",
+	//		test: testMultiHopPayments,
+	//	},
+	//	{
+	//		name: "private channels",
+	//		test: testPrivateChannels,
+	//	},
+	//	{
+	//		name: "multiple channel creation",
+	//		test: testBasicChannelCreation,
+	//	},
+	//	{
+	//		name: "invoice update subscription",
+	//		test: testInvoiceSubscriptions,
+	//	},
+	//	{
+	//		name: "multi-hop htlc error propagation",
+	//		test: testHtlcErrorPropagation,
+	//	},
+	//	// TODO(roasbeef): multi-path integration test
+	//	{
+	//		name: "node announcement",
+	//		test: testNodeAnnouncement,
+	//	},
+	//	{
+	//		name: "node sign verify",
+	//		test: testNodeSignVerify,
+	//	},
 	{
 		name: "async payments benchmark",
 		test: testAsyncPayments,
 	},
-	{
-		name: "async bidirectional payments",
-		test: testBidirectionalAsyncPayments,
-	},
-	{
-		// bob: outgoing our commit timeout
-		// carol: incoming their commit watch and see timeout
-		name: "test multi-hop htlc local force close immediate expiry",
-		test: testMultiHopHtlcLocalTimeout,
-	},
-	{
-		// bob: outgoing watch and see, they sweep on chain
-		// carol: incoming our commit, know preimage
-		name: "test multi-hop htlc receiver chain claim",
-		test: testMultiHopReceiverChainClaim,
-	},
-	{
-		// bob: outgoing our commit watch and see timeout
-		// carol: incoming their commit watch and see timeout
-		name: "test multi-hop local force close on-chain htlc timeout",
-		test: testMultiHopLocalForceCloseOnChainHtlcTimeout,
-	},
-	{
-		// bob: outgoing their commit watch and see timeout
-		// carol: incoming our commit watch and see timeout
-		name: "test multi-hop remote force close on-chain htlc timeout",
-		test: testMultHopRemoteForceCloseOnChainHtlcTimeout,
-	},
-	{
-		// bob: outgoing our commit watch and see, they sweep on chain
-		// bob: incoming our commit watch and learn preimage
-		// carol: incoming their commit know preimage
-		name: "test multi-hop htlc local chain claim",
-		test: testMultiHopHtlcLocalChainClaim,
-	},
-	{
-		// bob: outgoing their commit watch and see, they sweep on chain
-		// bob: incoming their commit watch and learn preimage
-		// carol: incoming our commit know preimage
-		name: "test multi-hop htlc remote chain claim",
-		test: testMultiHopHtlcRemoteChainClaim,
-	},
-	{
-		name: "switch circuit persistence",
-		test: testSwitchCircuitPersistence,
-	},
-	{
-		name: "switch offline delivery",
-		test: testSwitchOfflineDelivery,
-	},
-	{
-		name: "switch offline delivery persistence",
-		test: testSwitchOfflineDeliveryPersistence,
-	},
-	{
-		name: "switch offline delivery outgoing offline",
-		test: testSwitchOfflineDeliveryOutgoingOffline,
-	},
-	{
-		// TODO(roasbeef): test always needs to be last as Bob's state
-		// is borked since we trick him into attempting to cheat Alice?
-		name: "revoked uncooperative close retribution",
-		test: testRevokedCloseRetribution,
-	},
-	{
-		name: "revoked uncooperative close retribution zero value remote output",
-		test: testRevokedCloseRetributionZeroValueRemoteOutput,
-	},
-	{
-		name: "revoked uncooperative close retribution remote hodl",
-		test: testRevokedCloseRetributionRemoteHodl,
-	},
-	{
-		name: "query routes",
-		test: testQueryRoutes,
-	},
+	//	{
+	//		name: "async bidirectional payments",
+	//		test: testBidirectionalAsyncPayments,
+	//	},
+	//	{
+	//		// bob: outgoing our commit timeout
+	//		// carol: incoming their commit watch and see timeout
+	//		name: "test multi-hop htlc local force close immediate expiry",
+	//		test: testMultiHopHtlcLocalTimeout,
+	//	},
+	//	{
+	//		// bob: outgoing watch and see, they sweep on chain
+	//		// carol: incoming our commit, know preimage
+	//		name: "test multi-hop htlc receiver chain claim",
+	//		test: testMultiHopReceiverChainClaim,
+	//	},
+	//	{
+	//		// bob: outgoing our commit watch and see timeout
+	//		// carol: incoming their commit watch and see timeout
+	//		name: "test multi-hop local force close on-chain htlc timeout",
+	//		test: testMultiHopLocalForceCloseOnChainHtlcTimeout,
+	//	},
+	//	{
+	//		// bob: outgoing their commit watch and see timeout
+	//		// carol: incoming our commit watch and see timeout
+	//		name: "test multi-hop remote force close on-chain htlc timeout",
+	//		test: testMultHopRemoteForceCloseOnChainHtlcTimeout,
+	//	},
+	//	{
+	//		// bob: outgoing our commit watch and see, they sweep on chain
+	//		// bob: incoming our commit watch and learn preimage
+	//		// carol: incoming their commit know preimage
+	//		name: "test multi-hop htlc local chain claim",
+	//		test: testMultiHopHtlcLocalChainClaim,
+	//	},
+	//	{
+	//		// bob: outgoing their commit watch and see, they sweep on chain
+	//		// bob: incoming their commit watch and learn preimage
+	//		// carol: incoming our commit know preimage
+	//		name: "test multi-hop htlc remote chain claim",
+	//		test: testMultiHopHtlcRemoteChainClaim,
+	//	},
+	//	{
+	//		name: "switch circuit persistence",
+	//		test: testSwitchCircuitPersistence,
+	//	},
+	//	{
+	//		name: "switch offline delivery",
+	//		test: testSwitchOfflineDelivery,
+	//	},
+	//	{
+	//		name: "switch offline delivery persistence",
+	//		test: testSwitchOfflineDeliveryPersistence,
+	//	},
+	//	{
+	//		name: "switch offline delivery outgoing offline",
+	//		test: testSwitchOfflineDeliveryOutgoingOffline,
+	//	},
+	//	{
+	//		// TODO(roasbeef): test always needs to be last as Bob's state
+	//		// is borked since we trick him into attempting to cheat Alice?
+	//		name: "revoked uncooperative close retribution",
+	//		test: testRevokedCloseRetribution,
+	//	},
+	//	{
+	//		name: "revoked uncooperative close retribution zero value remote output",
+	//		test: testRevokedCloseRetributionZeroValueRemoteOutput,
+	//	},
+	//	{
+	//		name: "revoked uncooperative close retribution remote hodl",
+	//		test: testRevokedCloseRetributionRemoteHodl,
+	//	},
+	//	{
+	//		name: "query routes",
+	//		test: testQueryRoutes,
+	//	},
 }
 
 // TestLightningNetworkDaemon performs a series of integration tests amongst a
